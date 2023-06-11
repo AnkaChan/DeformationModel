fbc9e1d549aa9b845e061c5d10d6604356fc06f2
Fangfei Lan <fangfei.lan@gmail.com>
2023-06-10 20:07:10 -0600

    added vortexslice flipped

--- S05_ReverseEngineeringOfMergeTree/Data/geodesics/flipping_vti.py
+++ S05_ReverseEngineeringOfMergeTree/Data/geodesics/flipping_vti.py
@@ -4,16 +4,19 @@ from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
 from pyevtk.hl import imageToVTK
 
-input_dir = "./MovingGaussian/"
-# output_dir = "./VortexSlice/monoMesh_8_10/"
-inputStartFile = "monoMesh_2.vtp"
-outfile = input_dir + "monoMesh_2_flipped"
-gridSize = (150, 150)
+input_dir = "./VortexSlice/"
+output_dir = "./VortexSlice/monoMesh_8_10_smoothed_padded/"
+inputStartFile = "monoMesh_010_smoothed_padded.npy"
+outfile = input_dir + "monoMesh_010_flipped"
+# gridSize = (192, 64)
 
 fig = plt.figure()
-input = pv.read(input_dir + inputStartFile)
+# input = pv.read(input_dir + inputStartFile)
+input_image = np.load(input_dir + inputStartFile)
+gridSize = input_image.shape
+
 # input.plot()
-input_image = np.array(input['Scalars_']).reshape(gridSize[1], gridSize[0]).T
+# input_image = np.array(input['Scalars_']).reshape(gridSize[1], gridSize[0]).T
 input_image_flipped = -1*input_image
 ax1 = fig.add_subplot(121)
 ax2 = fig.add_subplot(122)
@@ -23,4 +26,4 @@ plt.show()
 
 input_pts = input_image.reshape(gridSize[0], gridSize[1], 1)
 flipped_pts = input_image_flipped.reshape(gridSize[0], gridSize[1], 1)
-imageToVTK(outfile, pointData={"Scalars_": input_pts, "Scalars_neg": flipped_pts})
+imageToVTK(outfile, pointData={"speed": input_pts, "speed_neg": flipped_pts})
--- S05_ReverseEngineeringOfMergeTree/Data/geodesics/geodesic_wrapper.py
+++ S05_ReverseEngineeringOfMergeTree/Data/geodesics/geodesic_wrapper.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
         t = i / 100
         print(t)
 
-        input_dir = "./MovingGaussian/"
-        output_dir = "./MovingGaussian/monoMesh_0_2_flipped/"
-        inputStartFile = "monoMesh_0_flipped.vti"
-        inputEndFile = "monoMesh_2_flipped.vti"
-        threshold = 0.01
+        input_dir = "./VortexSlice/"
+        output_dir = "./VortexSlice/monoMesh_8_10_flipped/"
+        inputStartFile = "monoMesh_008_flipped.vti"
+        inputEndFile = "monoMesh_010_flipped.vti"
+        threshold = 0.02
         absoluteParam = 0  # 0 - relative persisththt7ht7hh7tht7ht7tht7tence threshold (1%) 1 - absolute persistence threshold (0.06)
         print(absoluteParam)
         treeType = "st"
-        scalarField = 'Scalars_neg'
+        scalarField = 'speed_neg'
 
         submitCommand = "pvpython geodesic_other.py " + str(t) + " " + input_dir + \
                         " " + output_dir + " " + inputStartFile + " " + inputEndFile + " " + \
